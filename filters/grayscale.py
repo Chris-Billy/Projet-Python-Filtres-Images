39580c26bcccee769d936ee7a749a350a479ab2b
Christopher Billy <billy_christopher@hotmail.com>
2020-12-02 15:09:46 +0100

    Factorisation grayscale

--- filters/grayscale.py
+++ filters/grayscale.py
@@ -3,30 +3,5 @@ from logger import log
 import os
 
 def grayscale(img):
-
-    img_dest = f"imgs/{img}"
-    new_dest = "output/"
-    # On vérifie se type de l'image est correct
-    if img_dest.lower().endswith((".jpg", ".png", ".jpeg")):
-        # On essaie de convertir l'image
-        try:
-            image = cv2.imread(img_dest)
-            img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-            new_img = f"{new_dest}{img}"
-            # On vérifie si le dossier n'existe pas, on le cree
-            if not os.path.exists(new_dest):
-                os.makedirs(new_dest)
-            cv2.imwrite(new_img, img_gray)
-
-            log(f"Gray Scale => Conversion de l'image {img} en Noir et Blanc, enregistre dans {new_img}")
-        # Leve une erreur si le nom du fichier n'existe pas
-        except cv2.error:
-            print("Le fichier est introuvable ou n'existe pas")
-            log(f"Gray Scale => Tentative echouee, le fichier est introuvable ou n'existe pas")
-    # Ce n'est pas une image
-    else:
-        print("Veuillez saisir un fichier de type image (.jpg, .png)")
-        log(f"Gray Scale => Tentative echouee, le type du fichier est incorrect")
-
-# grayscale("tigre.jpg")
+    log(f"Dilate => Conversion de l'image avec un gris")
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 import os
+import cv2
+from logger import log
 from filters import grayscale as g, blur as b, dilate as d
+import sys
 
-img_dest = "imgs"
+args = sys.argv
+print(args)
 
-for image in os.listdir(img_dest):
-    g.grayscale(image)
+img_dest = args[1]
+
+for img_file in os.listdir(img_dest):
+    # g.grayscale(image)
     # b.blur(image, 15)
     # d.dilate(image, 15)
+    img_path = f"{img_dest}/{img_file}"
+    new_dest = f"output/"
+    # On vérifie se type de l'image est correct
+    if img_path.lower().endswith((".jpg", ".png", ".jpeg")):
+        # On essaie de convertir l'image
+        try:
+            image = cv2.imread(img_path)
+            image = g.grayscale(image)
+            new_img = f"{new_dest}{img_file}"
+            # On vérifie si le dossier n'existe pas, on le cree
+            if not os.path.exists(new_dest):
+                os.makedirs(new_dest)
+            cv2.imwrite(new_img, image)
+
+            # Leve une erreur si le nom du fichier n'existe pas
+        except cv2.error:
+            print("Le fichier est introuvable ou n'existe pas")
+            log(f"Dilate => Tentative echouee, le fichier est introuvable ou n'existe pas")
+            # Ce n'est pas une image
+    else:
+        print("Veuillez saisir un fichier de type image (.jpg, .png)")
+        log(f"Dilate => Tentative echouee, le type du fichier est incorrect")
