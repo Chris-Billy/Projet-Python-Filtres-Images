748f2f5f9f23bc814dff5151a999fec2fe8d0c72
Axel Demorest <axe.demorest@gmail.com>
2020-12-03 13:36:06 +0100

    ieieieie

--- main.py
+++ main.py
@@ -25,6 +25,25 @@ for arg in args:
     elif arg == "-o":
         output_folder = f"{args[i+1]}/"
 
+    elif arg == "--filters":
+        split_arg = args[i+1].split("|")
+        for split_arg_filter in split_arg:
+            split_arg_filter_list = split_arg_filter.split(":")
+            if len(split_arg_filter_list) == 2:
+                print(split_arg_filter_list[1])
+
+
+
     i += 1
 
-f.application_filter(entry_folder, output_folder)
+#print(split_arg)
+
+if "moi" in split_arg:
+    print("coucou")
+
+# Séparer l'argument avec les filtres avec "|"
+# Séparer l'argument du filtre avec ":"
+# Savoir si un des mots correspondant à un filtre est dans la liste et donc appliquer le filtre
+
+#f.application_filter(entry_folder, output_folder)
+
